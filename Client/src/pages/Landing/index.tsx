a30e119ffcab5358a6c1af6a8b15a1445fb12021
Eric Ponce <ponceeric458@gmail.com>
2023-06-06 20:49:56 -0700

    useSelect test

--- Client/src/pages/Landing/index.tsx
+++ Client/src/pages/Landing/index.tsx
@@ -1,8 +1,19 @@
 
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import { getInitialPokemonData } from "../../app/reducers/getInitialPokemonData";
 
 const Landing = () => {
 
+  const dispatch =  useDispatch();
 
+  const test = useSelector((state:RootState) =>  getInitialPokemonData())
+  
+
+  useEffect( () => {
+
+  })
 
 
     return (
