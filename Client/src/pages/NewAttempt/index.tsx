fd5be157ddfabc25a296e07579c6820f1d33c5dd
Eric Ponce <ponceeric458@gmail.com>
2023-06-14 20:07:44 -0700

    steps for new attempt

--- Client/src/pages/NewAttempt/index.tsx
+++ Client/src/pages/NewAttempt/index.tsx
@@ -1,68 +1,36 @@
-import { useState, useEffect } from "react";
-import { getPokemonGenerations, getPokemonRegionData } from "../../utils";
+import { useState } from "react";
+import { SelectGen, SelectGame, AddPlayers } from "./components";
 
 const NewAttempt = () => {
-  const [pokemonGens, setPokemonGens] = useState([]);
   const [selectedGen, setSelectedGen] = useState<string>("1");
-
-  const [regionData, setRegionData] = useState({ version_groups: [] });
   const [gameVersion, setGameVersion] = useState<string>("");
-
   const [players, setPlayers] = useState<Array<string>>([]);
-  const [playerUsername, setPlayerUsername] = useState<string>("");
-
-  const fetchPokemonGenerations = async () => {
-    const pokemonGenerations = await getPokemonGenerations();
-    console.log(pokemonGenerations.data.results);
-    setPokemonGens(pokemonGenerations.data.results);
-  };
-
-  const fetchPokemonRegionData = async () => {
-    const pokemonRegionData = await getPokemonRegionData(selectedGen);
-    console.log(pokemonRegionData.data);
-    setRegionData(pokemonRegionData.data);
-  };
-
-  useEffect(() => {
-    fetchPokemonGenerations();
-  }, []);
+  const [currentStep, setCurrentStep] = useState<number>(1);
 
   return (
-    <div className="h-full w-full bg-[#050511]">
-      <input type="text" />
-      <select onChange={(e) => setSelectedGen(e.target.value)}>
-        {pokemonGens.map((gen, idx) => (
-          <option key={idx} value={1 + idx}>
-            {gen["name"]}
-          </option>
-        ))}
-      </select>
-      <button onClick={fetchPokemonRegionData} className="text-white">
-        Some Button
-      </button>
-      <select onChange={(e) => setGameVersion(e.target.value)}>
-        {regionData.version_groups.map((version, idx) => (
-          <option key={idx}>{version["name"]}</option>
-        ))}
-      </select>
-      <input
-        type="text"
-        value={playerUsername}
-        onChange={(e) => setPlayerUsername(e.target.value)}
-      />
-      <button
-        className="text-[white]"
-        onClick={() => {
-          setPlayers([...players, playerUsername]);
-          setPlayerUsername("");
-        }}
-      >
-        Add Player
-      </button>
-
-      <button onClick={() => console.log(players)} className="text-white">
-        See Players
-      </button>
+    <div className="h-full w-full bg-[#050511] flex justify-center items-center">
+      <div className="h-64 w-[400px] bg-white rounded p-6">
+        {currentStep === 1 && (
+          <SelectGen
+            setSelectedGen={setSelectedGen}
+            setCurrentStep={setCurrentStep}
+          />
+        )}
+        {currentStep === 2 && (
+          <SelectGame
+            setCurrentStep={setCurrentStep}
+            selectedGen={selectedGen}
+            setGameVersion={setGameVersion}
+          />
+        )}
+        {currentStep === 3 && (
+          <AddPlayers
+            setCurrentStep={setCurrentStep}
+            players={players}
+            setPlayers={setPlayers}
+          />
+        )}
+      </div>
     </div>
   );
 };
