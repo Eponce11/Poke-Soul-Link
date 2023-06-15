605c246728be9368871269392cb768eface4a634
Eric Ponce <ponceeric458@gmail.com>
2023-06-14 20:08:44 -0700

    add players component

---
+++ Client/src/pages/NewAttempt/components/AddPlayers/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const AddPlayers = (props: any) => {
+  const { setCurrentStep, players, setPlayers } = props;
+
+  const [playerUsername, setPlayerUsername] = useState<string>("");
+
+  const handleAddingPlayer = (e: any) => {
+    e.preventDefault();
+    setPlayers((prev: any) => [...prev, playerUsername]);
+    setPlayerUsername("");
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => setPlayerUsername(e.target.value)}
+        value={playerUsername}
+      />
+      <ol>
+        {players.map( (player:any, idx:number) => (
+          <li key={idx}>{player}</li>
+        ))}
+      </ol>
+      <button onClick={handleAddingPlayer}>Add</button>
+      <button onClick={() => setCurrentStep((prev: number) => prev - 1)}>
+        Back
+      </button>
+    </>
+  );
+};
+
+export default AddPlayers;
